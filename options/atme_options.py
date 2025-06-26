adbed06a076acef23dacadb338f2c1039f43684d
RotemBenisty <154740550+RotemBenisty@users.noreply.github.com>
2025-06-26 14:01:11 +0300

    Add files via upload

--- options/atme_options.py
+++ options/atme_options.py
@@ -62,6 +62,4 @@ class AtmeOptions(BaseOptions):
         parser.add_argument('--lr_policy', type=str, default='linear', help='learning rate policy. [linear | step | plateau | cosine]')
         parser.add_argument('--lr_decay_iters', type=int, default=50, help='multiply by a gamma every lr_decay_iters iterations')
 
-        # self.isTrain = True
-
         return parser
